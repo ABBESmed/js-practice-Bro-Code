ff6c022b3ea3f71966fb4b7aa892a158b3975182
Mohammed Zoheir Abbes <mohammedzoheirabbes@gmail.com>
2026-07-06 00:02:18 +0200

    random number generator

---
+++ JS9/index.js
@@ -0,0 +1,19 @@
+/*let randomNum = Math.floor(Math.random() * 6 + 1);
+console.log(randomNum);
+
+const min = 50;
+const max = 100;
+
+let rNum = Math.floor(Math.random() * (max - min) + min);
+console.log(rNum);*/
+
+const myButton = document.getElementById("myButton");
+const myLabel = document.getElementById("myLabel");
+const min = 1;
+const max = 6;
+let randomNum;
+
+myButton.onclick = function () {
+  randomNum = Math.floor(Math.random() * max) + min;
+  myLabel.textContent = randomNum;
+};
