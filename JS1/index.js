144a420471f9b0ff87cc33f50ad127418e071d73
Mohammed Zoheir Abbes <mohammedzoheirabbes@gmail.com>
2026-07-05 18:32:35 +0200

    console alert document

---
+++ JS1/index.js
@@ -0,0 +1,16 @@
+console.log("Hello");
+console.log("I like Pizza!");
+
+alert("This is an alert!!!");
+alert("I like Pizza!!!!!!!!!!!!!");
+
+// This a single line commment
+
+/* This
+    a multi
+    line
+    comment
+*/
+
+document.getElementById("myH1").textContent = "haha";
+document.getElementById("myP").textContent = "yooo";
