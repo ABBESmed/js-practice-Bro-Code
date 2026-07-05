f6747b01f5dd2e046ad4fc825c91fb1da9140fa2
Mohammed Zoheir Abbes <mohammedzoheirabbes@gmail.com>
2026-07-05 19:49:01 +0200

    Variables

---
+++ JS2/index.js
@@ -0,0 +1,43 @@
+// Varibles , declaration let x;  assignment x = 100;
+let x; // this is declaration of variable.
+let y;
+x = 200; // assigne a value like here for example 200.
+y = 99;
+
+let z = 69; // here simple way declaration + assignment in one line.
+
+let age = 28;
+console.log(age);
+console.log("You are " + age + " years old.");
+console.log(typeof age);
+
+let price = 999.99;
+console.log(price);
+console.log("The price is: " + price + " euro.");
+console.log(typeof price);
+
+let gradePointAverage = 3.5;
+console.log(gradePointAverage);
+console.log("Your gpa is: " + gradePointAverage);
+console.log(typeof gradePointAverage);
+
+let lastName = "Abbes";
+let firstName = "Mohammed Zoheir";
+console.log(typeof (lastName, firstName));
+console.log("Welcome Mr." + lastName + " " + firstName);
+
+let online = true;
+console.log(online);
+console.log(typeof online);
+
+let spendMoneyToday = false;
+console.log(typeof spendMoneyToday);
+console.log(spendMoneyToday);
+
+let fullName = "Glareful";
+let AGE = 28;
+let student = false;
+
+document.getElementById("p1").textContent = fullName;
+document.getElementById("p2").textContent = AGE;
+document.getElementById("p3").textContent = student;
