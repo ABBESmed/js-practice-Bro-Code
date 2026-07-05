3f00b59f5bc1aa819ec521349e889cfacdb31bb1
Mohammed Zoheir Abbes <mohammedzoheirabbes@gmail.com>
2026-07-05 21:29:48 +0200

    TYPE CONVERSION

---
+++ JS5/index.js
@@ -0,0 +1,19 @@
+let age = prompt("How old are you?");
+age = Number(age);
+age += 1;
+console.log(age, typeof age);
+
+let x = "pizza";
+let y = "pizza";
+let z = "pizza";
+let t = "";
+
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+t = Boolean(t);
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
+console.log(t, typeof t);
