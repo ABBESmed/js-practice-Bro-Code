564aeff0e4c1515a6f15571bc6c4e0b604979ef1
Mohammed Zoheir Abbes <mohammedzoheirabbes@gmail.com>
2026-07-05 20:23:09 +0200

    ARITHMETIC OPERATORS

---
+++ JS3/index.js
@@ -0,0 +1,37 @@
+// operands <=> values , variables .....
+// operations <=> + - / * %
+
+let students = 10;
+// new student join the class so we add +1
+
+students = students + 1; // this is add operator +
+students = students - 1; // this is sub operator -
+students = students * 2; // this is mul operator *
+students = students / 2; // this is div operator /
+students = students ** 3; // this is exp operator **
+students = students % 100; // this is modulo operator %
+console.log(students);
+
+//augmented assignment operators
+let numberOfIphones = 20;
+numberOfIphones += 1;
+console.log(numberOfIphones);
+numberOfIphones -= 1;
+console.log(numberOfIphones);
+numberOfIphones *= 2;
+console.log(numberOfIphones);
+numberOfIphones /= 2;
+console.log(numberOfIphones);
+numberOfIphones **= 3;
+console.log(numberOfIphones);
+numberOfIphones %= 100;
+console.log(numberOfIphones);
+
+// increment decrement
+
+let ram = 1;
+ram++; // increment
+console.log(ram);
+
+ram--; // decrement
+console.log(ram);
